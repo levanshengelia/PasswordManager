ef479f86b9edc313dd03f8f5c6b537735925834b
levanshengelia <lshen20@freeuni.edu.ge>
2024-08-04 22:06:17 +0400

    add gitignore

--- Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core;
-
-public interface ICore 
-{
-	public bool RegisterUser(string email, string userName, string password);
-	public bool AuthorizeUser(string userName, string password);
-	public string GetPassword(string userName, string password, string appName);
-}
