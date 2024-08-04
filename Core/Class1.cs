2cf2363457a42bfd6a0757aecf0088a7a058f7ba
levanshengelia <lshen20@freeuni.edu.ge>
2024-08-04 11:40:01 +0400

    Create projects

---
+++ Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace Core;
+
+public interface ICore 
+{
+	public bool RegisterUser(string email, string userName, string password);
+	public bool AuthorizeUser(string userName, string password);
+	public string GetPassword(string userName, string password, string appName);
+}
